f580d61bdf9d128395a2ad3ed1a3ae46e8cdae8b
Pierre Colle <piercus@gmail.com>
2022-09-19 14:34:59 +0200

    fix: xo, README.md ...

--- README.md
+++ README.md
@@ -23,5 +23,67 @@ readStream.pipe(writeStream);
 will output 
 
 ```
-
+{
+  threadId: 0,
+  latency: 11,
+  markers: [
+    {
+      markerName: 'read',
+      date: '2022-09-19T12:29:00.021Z',
+      threadId: 0,
+      streamName: 'readStream'
+    },
+    {
+      markerName: 'push',
+      date: '2022-09-19T12:29:00.021Z',
+      threadId: 0,
+      streamName: 'readStream'
+    },
+    {
+      markerName: 'write',
+      date: '2022-09-19T12:29:00.025Z',
+      threadId: 0,
+      streamName: 'writeStream'
+    },
+    {
+      markerName: 'callback',
+      date: '2022-09-19T12:29:00.032Z',
+      threadId: 0,
+      streamName: 'writeStream'
+    }
+  ],
+  throughput: 1
+}
+...
+{
+  threadId: 43,
+  latency: 6,
+  markers: [
+    {
+      markerName: 'read',
+      date: '2022-09-19T12:29:00.224Z',
+      threadId: 43,
+      streamName: 'readStream'
+    },
+    {
+      markerName: 'push',
+      date: '2022-09-19T12:29:00.228Z',
+      threadId: 43,
+      streamName: 'readStream'
+    },
+    {
+      markerName: 'write',
+      date: '2022-09-19T12:29:00.229Z',
+      threadId: 43,
+      streamName: 'writeStream'
+    },
+    {
+      markerName: 'callback',
+      date: '2022-09-19T12:29:00.230Z',
+      threadId: 43,
+      streamName: 'writeStream'
+    }
+  ],
+  throughput: 44
+}
 ```
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const {PassThrough} = require('stream');
 const test = require('ava');
 const tracify = require('../lib/index.js');
-// 
+//
 // process.on('uncaughtException', function(err) {
 //   console.log(err.stack);
 //   throw err;
@@ -13,37 +13,39 @@ test('simple usage 2', t => {
 	const writeStream = tracify(fs.createWriteStream('./tmp/file.txt'), {name: 'writeStream'});
 	let n = 0;
 	writeStream.on('trace', info => {
+		console.log(info);
 		t.is(info.threadId, n);
 		t.true((info.latency < 50));
 		if (n > 20) {
 			t.true((info.throughput > 10));
 		}
-	
+
 		t.is(info.markers.length, 4);
-	
+
 		t.is(info.markers[0].streamName, 'readStream');
 		t.is(info.markers[0].markerName, 'read');
-	
+
 		t.is(info.markers[1].streamName, 'readStream');
 		t.is(info.markers[1].markerName, 'push');
-	
+
 		t.is(info.markers[2].streamName, 'writeStream');
 		t.is(info.markers[2].markerName, 'write');
-	
+
 		t.is(info.markers[3].streamName, 'writeStream');
 		t.is(info.markers[3].markerName, 'callback');
-	
+
 		n++;
 	});
-	
+
 	const outStream = readStream.pipe(writeStream);
-	
+
 	return new Promise((resolve, reject) => {
+		outStream.on('error', reject);
 		outStream.on('finish', () => {
 			t.true(n > 40);
-			resolve()
+			resolve();
 		});
-	})
+	});
 });
 
 test('simple usage with transform', t => {
@@ -52,42 +54,43 @@ test('simple usage with transform', t => {
 	const pass = tracify(new PassThrough(), {name: 'middle'});
 	let n = 0;
 	writeStream.on('trace', info => {
-		t.is(info.threadId, n+'-0');
+		t.is(info.threadId, n + '-0');
 		t.true((info.latency < 50));
 		if (n > 20) {
 			t.true((info.throughput > 10));
 		}
-	
+
 		t.is(info.markers.length, 6);
-	
+
 		t.is(info.markers[0].streamName, 'readStream');
 		t.is(info.markers[0].markerName, 'read');
-	
+
 		t.is(info.markers[1].streamName, 'readStream');
 		t.is(info.markers[1].markerName, 'push');
-	
+
 		t.is(info.markers[2].streamName, 'middle');
 		t.is(info.markers[2].markerName, 'transform');
-	
+
 		t.is(info.markers[3].streamName, 'middle');
 		t.is(info.markers[3].markerName, 'callback');
-		
+
 		t.is(info.markers[4].streamName, 'writeStream');
 		t.is(info.markers[4].markerName, 'write');
-	
+
 		t.is(info.markers[5].streamName, 'writeStream');
 		t.is(info.markers[5].markerName, 'callback');
-		
+
 		n++;
 	});
-	
+
 	const outStream = readStream.pipe(pass).pipe(writeStream);
-	
+
 	return new Promise((resolve, reject) => {
+		outStream.on('error', reject);
 		outStream.on('finish', () => {
 			t.true(n > 40);
-			resolve()
+			resolve();
 		});
-	})
+	});
 });
 
